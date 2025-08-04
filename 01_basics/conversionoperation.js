4011cb8be31deec88bc15f3f68c5501a565bdee9
3986abhishek <163152644+3986abhishek@users.noreply.github.com>
2025-08-04 13:53:25 +0000

    conversionsoperatjion

---
+++ 01_basics/conversionoperation.js
@@ -0,0 +1,19 @@
+let score="33ass"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// must be know that in conversion in a javascript some kind of issue check it clearly
+
+
+let isLoggedIn =50
+
+let booleanisLoggedIn=Boolean(isLoggedIn)
+
+console.log(booleanisLoggedIn);
+
+//1=>true 0=>false
