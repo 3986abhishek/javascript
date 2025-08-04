fd0682e6e7e16863937376180f3c053be79a84c7
3986abhishek <163152644+3986abhishek@users.noreply.github.com>
2025-08-04 10:41:59 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const customarId=12342
+let accounteEmail="abhi@google.com"
+var accountPassword="53242"
+account_city="jaipur"
+
+// customerId= 2 not allowed
+// console.log(customarId);
+
+accounteEmail="abjo@32323.com"
+accountPassword="2323"
+account_city="bangluru"
+
+
+console.table([customarId,accountPassword,accounteEmail,account_city])
